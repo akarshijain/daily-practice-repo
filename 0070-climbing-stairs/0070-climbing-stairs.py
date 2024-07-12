a2f94339b25df3037f805c3479d9bbecca1ccb8f
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-07-12 10:48:58 -0400

    Time: 29 ms (87.7%), Space: 16.5 MB (53.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,9 +1,12 @@
 class Solution:
     cache = {}
-    cache[1] = 1
-    cache[2] = 2
 
     def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
         if n not in self.cache:
             self.cache[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
 
