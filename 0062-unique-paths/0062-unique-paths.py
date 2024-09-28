734c80b5bb5e9f8c9ff327f3b892d25fdf55e61b
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-09-28 15:36:42 -0400

    Time: 35 ms (59.24%), Space: 16.5 MB (41.47%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,23 +1,9 @@
 class Solution:
-    def __init__(self):
-        self.cache = {}
-
-    def dfs(self, r, c, m, n):
-
-        if r not in range(m) or c not in range(n):
-            return 0
-
-        if (r, c) in self.cache:
-            return self.cache[(r, c)]
-
-        if r == m - 1 and c == n - 1:
-            return 1
-
-        self.cache[(r, c)] = self.dfs(r+1, c, m, n) + self.dfs(r, c+1, m, n)
-
-        return self.cache[(r, c)]
-
     def uniquePaths(self, m: int, n: int) -> int:
-        return self.dfs(0, 0, m, n)
+        dp = [[1] * (n+1)] * (m+1)
+     
+        for i in range(m-2, -1, -1):
+            for j in range(n-2, -1, -1):
+                dp[i][j] = dp[i][j+1] + dp[i+1][j]
 
-        
+        return dp[0][0]
