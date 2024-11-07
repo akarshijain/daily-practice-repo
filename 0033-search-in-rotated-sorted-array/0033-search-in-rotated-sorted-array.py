1dccb586dd5476269488f25f61d77f9a14e42354
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-11-07 15:52:37 -0500

    Time: 0 ms (100%), Space: 17.1 MB (15.55%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,24 +1,53 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def findPivot(self, nums):
+        pivot = 0
+
         left = 0
         right = len(nums) - 1
 
         while left <= right:
-
             mid = (left + right) // 2
-            if target == nums[mid]:
+
+            if nums[mid] <= nums[-1]:
+                pivot = mid
+                right = mid - 1
+
+            else:
+                left = mid + 1
+
+        return pivot
+
+    def binarySearch(self, nums: List[int], target: int, left: int, right: int) -> int:
+        print(nums)
+        
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
                 return mid
 
-            if nums[mid] >= nums[left]:
-                if nums[left] <= target <= nums[mid]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
+            if nums[mid] > target:
+                right = mid - 1
 
-            elif nums[mid] < nums[left]:
-                if nums[mid] <= target <= nums[right]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
+            else:
+                left = mid + 1
 
         return -1
+
+
+    def search(self, nums: List[int], target: int) -> int:
+        index = -1
+        pivot = self.findPivot(nums)
+
+        if target >= nums[pivot] and target <= nums[-1]:
+            index = self.binarySearch(nums, target, pivot, len(nums) - 1)
+
+        if index != -1:
+            return index
+        else:
+            return self.binarySearch(nums, target, 0, pivot - 1)
+
+        
+
+        
+        
