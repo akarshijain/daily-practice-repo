786b960f590b351f2c252359799e0bf192c03ec5
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-11-07 16:31:28 -0500

    Time: 0 ms (100%), Space: 20.2 MB (16.75%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,16 +6,27 @@
 #         self.right = right
 class Solution:
     def __init__(self):
-        self.inorder = []
-    
-    def inorderTraversal(self, root: Optional[TreeNode]):
+        self.result = -1
+        self.count = 0
+
+    def dfs(self, root):
         if not root:
             return
+
+        self.dfs(root.left)
+
+        self.count -= 1
+        if self.count == 0:
+            self.result = root.val
+            return
         
-        self.inorderTraversal(root.left)
-        self.inorder.append(root.val)
-        self.inorderTraversal(root.right)
+        self.dfs(root.right)
+
+        return
 
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        self.inorderTraversal(root)
-        return self.inorder[k-1]
+        self.count = k
+        self.dfs(root)
+        return self.result
+
+        
