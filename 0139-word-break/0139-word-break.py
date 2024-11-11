94ac74ed7d822c3a1c8d20a00101863b6ce2b734
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-11-11 15:13:30 -0500

    Time: 0 ms (100%), Space: 16.6 MB (90.66%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,15 +1,25 @@
 class Solution:
+    def __init__(self):
+        self.memo = {}
+
+    def dfs(self, index, s, wordDict): 
+        if index in self.memo:
+            return self.memo[index]
+
+        if index == len(s):
+            return True
+
+        for word in wordDict:
+            if s[index:].startswith(word):
+                if self.dfs(index + len(word), s, wordDict):
+                    self.memo[index] = True
+                    return True
+
+        self.memo[index] = False
+        return False
+
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [False] * (len(s) + 1)
-        dp[len(s)] = True
+        return self.dfs(0, s, wordDict)
 
-        for i in range(len(s), -1, -1):
-            for word in wordDict:
-                if (i + len(word)) <= len(s) and word == s[i:i+len(word)]:
-                    dp[i] = dp[i + len(word)]
-                if dp[i]:
-                    break
-                
-                
-        return dp[0]
 
+       
