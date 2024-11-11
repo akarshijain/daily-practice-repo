b0fce47dfcd4b303ac3e7c7a6ebc83cd1964130c
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-11-10 21:10:19 -0500

    Time: 1 ms (50.54%), Space: 16.6 MB (85.69%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,6 +2,7 @@ class Solution:
     def __init__(self):
         self.path = []
         self.permutations = []
+        self.lookup = set()
 
     def dfs(self, index, nums):
         if index >= len(nums):
@@ -9,10 +10,14 @@ class Solution:
             return
 
         for i in range(len(nums)):
-            if nums[i] not in self.path:
+            if nums[i] not in self.lookup:
                 self.path.append(nums[i])
+                self.lookup.add(nums[i])
+
                 self.dfs(index + 1, nums)
+
                 self.path.pop()
+                self.lookup.remove(nums[i])
         return
 
     def permute(self, nums: List[int]) -> List[List[int]]:
