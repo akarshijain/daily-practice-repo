819a0f896ee0c8050c5903beb35c1dc1398d299f
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-11-13 17:20:09 -0500

    Time: 7 ms (28.65%), Space: 16.7 MB (17.86%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,36 +1,38 @@
 class Solution:
+    def __init__(self):
+        self.fresh_oranges = 0
+        self.time = 0
+        self.orange_q = deque()
+
+
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh_oranges = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    fresh_oranges += 1
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1:
+                    self.fresh_oranges += 1
+                if grid[row][col] == 2:
+                    self.orange_q.append((row, col))
 
-        rotten_oranges = collections.deque()
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 2:
-                    rotten_oranges.append((i, j))
-
-        time = 0
-        while rotten_oranges and fresh_oranges:
-            for i in range(len(rotten_oranges)):
-                r, c = rotten_oranges.popleft()
-
-                directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        while self.fresh_oranges > 0 and self.orange_q:
+            for _ in range(len(self.orange_q)):
+                curr_row, curr_col = self.orange_q.popleft()
                 for dr, dc in directions:
-                    row, col = r + dr, c + dc
-                    if row in range(len(grid)) \
-                        and col in range(len(grid[0])) \
-                        and grid[row][col] == 1:
+                    new_row, new_col = curr_row + dr, curr_col + dc
 
-                        grid[row][col] = 2
-                        rotten_oranges.append((row, col))
-                        fresh_oranges -= 1
+                    if new_row not in range(len(grid)) or \
+                        new_col not in range(len(grid[0])) or \
+                        grid[new_row][new_col] == 0 or \
+                        grid[new_row][new_col] == 2:
+                        continue
 
-            time += 1
+                    grid[new_row][new_col] = 2
+                    self.fresh_oranges -= 1
+                    self.orange_q.append((new_row, new_col))
 
-        if fresh_oranges:
-            return -1
-
-        return time
+            self.time += 1
+        
+        if self.fresh_oranges == 0:
+            return self.time
+        
+        return -1
