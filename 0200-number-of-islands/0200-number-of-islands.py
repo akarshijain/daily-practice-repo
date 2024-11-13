cd0add75d03608d5e6f0f7e20eacc45a8d32ec9d
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-11-13 13:27:33 -0500

    Time: 321 ms (6.23%), Space: 24.3 MB (28.25%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,33 +1,35 @@
 class Solution:
     def __init__(self):
-        self.visit = set()
         self.count = 0
-    
-    def bfs(self, grid, r, c):
-        island_q = collections.deque()
-        island_q.append((r, c))
+        self.visited = set()
+        self.island_q = deque()
 
-        self.visit.add((r, c))
+    def countIslands(self, row, col, grid):
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
-        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+        self.island_q.append((row, col))
+        self.visited.add((row, col))
 
-        while island_q:
-            for i in range(len(island_q)):
-                r, c = island_q.popleft()
-                for dr, dc in directions:
-                    row, col = r + dr, c + dc
-                    if row in range(len(grid)) \
-                            and col in range(len(grid[0])) \
-                            and grid[row][col] == "1" \
-                            and (row, col) not in self.visit:
-                        island_q.append((row, col))
-                        self.visit.add((row, col))
+        while self.island_q:
+            old_row, old_col = self.island_q.popleft()
+            for dr, dc in directions:
+                new_row = old_row + dr
+                new_col = old_col + dc
+
+                if new_row not in range(len(grid)) or \
+                    new_col not in range(len(grid[0])) or \
+                    (new_row, new_col) in self.visited or \
+                    grid[new_row][new_col] == "0":
+                    continue
+
+                self.island_q.append((new_row, new_col))
+                self.visited.add((new_row, new_col))
 
     def numIslands(self, grid: List[List[str]]) -> int:
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == "1" and (r, c) not in self.visit:
-                    self.bfs(grid, r, c)
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == "1" and (row, col) not in self.visited:
                     self.count += 1
+                    self.countIslands(row, col, grid)
 
         return self.count
