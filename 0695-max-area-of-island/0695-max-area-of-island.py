018e8498d0170d8de86c43f25212eff97c09d3ac
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-11-13 15:09:30 -0500

    Time: 43 ms (12.14%), Space: 17.2 MB (71.98%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,38 +1,37 @@
 class Solution:
     def __init__(self):
-        self.area = 0
-        self.visit = set()
+        self.max_area = 0
+        self.visited = set()
+        self.island_q = deque()
 
-    def bfs(self, row, col, grid):
+    def calculateIslandArea(self, row, col, grid):
         area = 1
-        island = collections.deque()
 
-        island.append((row, col))
-        self.visit.add((row, col))
+        self.island_q.append((row, col))
+        self.visited.add((row, col))
 
-        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-
-        while island:
-            r, c = island.popleft()
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        while self.island_q:
+            curr_row, curr_col = self.island_q.popleft()
             for dr, dc in directions:
-                row, col = r + dr, c + dc
+                new_row, new_col = curr_row + dr, curr_col + dc
 
-                if row in range(len(grid)) \
-                    and col in range(len(grid[0])) \
-                    and grid[row][col] == 1 \
-                    and (row, col) not in self.visit:
+                if new_row not in range(len(grid)) or \
+                    new_col not in range(len(grid[0])) or \
+                    (new_row, new_col) in self.visited or \
+                    grid[new_row][new_col] == 0:
+                    continue
 
-                    island.append((row, col))
-                    self.visit.add((row, col))
-
-                    area += 1
-
+                area += 1
+                self.island_q.append((new_row, new_col))
+                self.visited.add((new_row, new_col))
+                
         return area
 
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        for i in range(len(grid)):
-            for j in range (len(grid[0])):
-                if grid[i][j] == 1 and (i, j) not in self.visit:
-                    self.area = max(self.area, self.bfs(i, j, grid))
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1 and (row, col) not in self.visited:
+                    self.max_area = max(self.max_area, self.calculateIslandArea(row, col, grid))
 
-        return self.area
+        return self.max_area
