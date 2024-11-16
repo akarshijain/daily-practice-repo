9f4647c62c9195287febbbbc10e312a5af750aff
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-11-16 11:11:06 -0500

    Time: 68 ms (54.67%), Space: 17.2 MB (17.73%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -5,31 +5,27 @@ class Codec:
         encoded_str = ""
 
         for word in strs:
-            encoded_str += str(len(word))
-            encoded_str += '#'
-            encoded_str += word
-        
-        return encoded_str
+            encoded_str += str(len(word)) + "#" + word
+            # include hash for word lengths that are grater than 1 digit
 
+        return encoded_str
+        
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
         decoded_str = []
 
-        index = 0
-        while index < len(s):
-            if s[index].isnumeric():
-                word_len = ''
-                while(s[index] != '#'):
-                    word_len += s[index]
-                    index += 1
-                start = index + 1
-                end = index + 1 + int(word_len)
-                decoded_str.append(s[start:end])
-                index = end
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            word_len = int(s[i : j])
+            decoded_str.append(s[j + 1 : j + 1 + word_len])
+
+            i = j + 1 + word_len
 
         return decoded_str
-
 
 
 # Your Codec object will be instantiated and called as such:
