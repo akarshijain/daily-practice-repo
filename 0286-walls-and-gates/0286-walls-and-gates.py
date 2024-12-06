bfce688bebe4fc4c39a10cca66697d7744dc4286
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-12-06 16:19:09 -0500

    Time: 327 ms (11.44%), Space: 20.2 MB (83.75%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -1,32 +1,37 @@
 class Solution:
     def __init__(self):
-        self.room_q = deque()
-
-    def calculateNearestGate(self, row, col, grid):
-        level = 1
+        pass
+    
+    def fillEmptyRooms(self, row, col, grid):
         visited = set()
-        self.room_q.append((row, col))
-        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        visited.add((row, col))
 
-        while self.room_q:
-            for _ in range(len(self.room_q)):
-                curr_row, curr_col = self.room_q.popleft()
+        rooms_q = deque()
+        rooms_q.append((row, col))
+
+        distance = 1
+
+        while rooms_q:
+            for i in range(len(rooms_q)):
+                old_row, old_col = rooms_q.popleft()
+
+                directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
                 for dr, dc in directions:
-                    new_row, new_col = curr_row + dr, curr_col + dc
+                    new_row, new_col = old_row + dr, old_col + dc
 
-                    if new_row not in range(len(grid)) or \
-                        new_col not in range(len(grid[0])) or \
-                        (new_row, new_col) in visited or \
-                        grid[new_row][new_col] == -1 or \
-                        grid[new_row][new_col] == 0:
-
+                    if new_row not in range(len(grid)) \
+                        or new_col not in range(len(grid[0])) \
+                        or (new_row, new_col) in visited \
+                        or grid[new_row][new_col] == -1 \
+                        or grid[new_row][new_col] == 0:
                         continue
 
-                    grid[new_row][new_col] = min(grid[new_row][new_col], level)
-                    self.room_q.append((new_row, new_col))
                     visited.add((new_row, new_col))
-            level += 1
+                    rooms_q.append((new_row, new_col))
+                    grid[new_row][new_col] = min(grid[new_row][new_col], distance)
 
+            distance += 1
+            
 
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
         """
@@ -35,5 +40,5 @@ class Solution:
         for row in range(len(rooms)):
             for col in range(len(rooms[0])):
                 if rooms[row][col] == 0:
-                    self.calculateNearestGate(row, col, rooms)
+                    self.fillEmptyRooms(row, col, rooms)
         
