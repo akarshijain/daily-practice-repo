d7f64f6d25ef321aecc9587131b14e4be63b564f
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-12-22 14:29:14 -0500

    Time: 0 ms (100%), Space: 17.8 MB (6.43%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def __init__(self):
-        self.keypad = {
+        self.num_map = {
             "2": "abc",
             "3": "def",
             "4": "ghi",
@@ -8,29 +8,28 @@ class Solution:
             "6": "mno",
             "7": "pqrs",
             "8": "tuv",
-            "9": "wxyz",
+            "9": "wxyz"
         }
+        self.combination = []
+        self.all_combinations = []
 
-        self.result = []
-        self.subset = []
-
-    def getLetterCombinations(self, index, digits):
+    def getCombination(self, index, digits):
         if index >= len(digits):
-            self.result.append("".join(self.subset.copy()))
+            self.all_combinations.append(''.join(self.combination.copy()))
             return
 
-        letters = self.keypad[digits[index]]
+        letters = self.num_map[digits[index]]
+
         for letter in letters:
-            self.subset.append(letter)
-            self.getLetterCombinations(index + 1, digits)
-            self.subset.pop()
+            self.combination.append(letter)
+            self.getCombination(index + 1, digits)
+            self.combination.pop()
 
         return
 
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-
-        self.getLetterCombinations(0, digits)
-        return self.result
-        
+            
+        self.getCombination(0, digits)
+        return self.all_combinations
