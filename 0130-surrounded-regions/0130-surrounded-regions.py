9af8c5a6dfea2643eb8d413c90fdc4c889bfd9c1
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-12-23 14:12:58 -0500

    Time: 978 ms (5.08%), Space: 21.9 MB (21.3%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,36 +1,61 @@
 class Solution:
+    def capture_unsurrounded_regions(self, row, col, board):
+        visited = set()
+        visited.add((row, col))
+
+        cell_q = deque()
+        cell_q.append((row, col))
+
+        board[row][col] = "#"
+
+        while cell_q:
+            old_row, old_col = cell_q.popleft()
+            
+            directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+            for dr, dc in directions:
+                new_row, new_col = old_row + dr, old_col + dc
+
+                if new_row not in range(len(board)) \
+                    or new_col not in range(len(board[0])) \
+                    or (new_row, new_col) in visited \
+                    or board[new_row][new_col] == "X":
+                    continue
+
+                cell_q.append((new_row, new_col))
+                visited.add((new_row, new_col))
+                board[new_row][new_col] = "#"
+                
+
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        cellq = deque()
+        unsurrounded_regions = set()
+        for col in range(len(board[0])):
+            if board[0][col] == "O":
+                unsurrounded_regions.add((0, col))
+
+            if board[-1][col] == "O":
+                unsurrounded_regions.add((len(board) - 1, col))
 
         for row in range(len(board)):
-            for col in range(len(board[0])):
-                if (row == 0 or row == len(board) - 1 or \
-                    col == 0 or col == len(board[0]) - 1) and \
-                    board[row][col] == "O":
-                    cellq.append((row, col))
+            if board[row][0] == "O":
+                unsurrounded_regions.add((row, 0))
 
-                    
+            if board[row][-1] == "O":
+                unsurrounded_regions.add((row, len(board[0]) - 1))
         
-        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-        while cellq:
-            old_row, old_col = cellq.popleft()
-            board[old_row][old_col] = "T"
-            for dr, dc in directions:
-                new_row, new_col = old_row + dr, old_col + dc
-                
-                if new_row in range(len(board)) and \
-                    new_col in range(len(board[0])) and \
-                    board[new_row][new_col] == "O":
-
-                    board[new_row][new_col] = "T"
-                    cellq.append((new_row, new_col))
+        for row, col in unsurrounded_regions:
+            self.capture_unsurrounded_regions(row, col, board)
 
         for row in range(len(board)):
             for col in range(len(board[0])):
                 if board[row][col] == "O":
                     board[row][col] = "X"
-                elif board[row][col] == "T":
+
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == "#":
                     board[row][col] = "O"
+
+        
