3b3e9206c597d6aa49715197e401972d9a48cfd0
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-12-23 16:43:07 -0500

    Time: 8 ms (26.15%), Space: 19.2 MB (9.75%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,33 +1,30 @@
 class Solution:
     def __init__(self):
         self.visited = set()
+    
+    def dfs(self, curr_node, prev_node, adj_list):
+        self.visited.add(curr_node)
 
-    def dfs(self, node, prev, neighbors_map):
-        self.visited.add(node)
-
-        for nei in neighbors_map[node]:
-            if nei == prev or nei in self.visited:
+        for neighbor in adj_list[curr_node]:
+            if neighbor == prev_node or neighbor in self.visited:
                 continue
 
-            self.dfs(nei, node, neighbors_map)
-
-        return 
+            self.dfs(neighbor, curr_node, adj_list)
 
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        neighbors_map = {}
-
+        adj_list = {}
         for i in range(n):
-            neighbors_map[i] = []
+            adj_list[i] = []
 
-        for node, nei in edges:
-            neighbors_map[node].append(nei)
-            neighbors_map[nei].append(node)
+        for node, neighbor in edges:
+            adj_list[node].append(neighbor)
+            adj_list[neighbor].append(node)
 
         count = 0
-        for node in neighbors_map:
-            if node in self.visited:
+        for i in range(n):
+            if i in self.visited:
                 continue
             count += 1
-            self.dfs(node, -1, neighbors_map)
+            self.dfs(i, -1, adj_list)
 
         return count
