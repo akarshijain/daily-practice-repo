c522d685a98e857185419adb2ee6fdb83c17bcb8
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-12-23 16:31:31 -0500

    Time: 2 ms (53.31%), Space: 18.9 MB (15.56%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,39 +1,32 @@
 class Solution:
     def __init__(self):
         self.visited = set()
-
-    def dfs(self, node, prev, adj_list):
-        if node in self.visited:
+    
+    def dfs(self, curr_node, prev_node, adj_list):
+        if curr_node in self.visited:
             return False
-            
-        self.visited.add(node)
 
-        for nei in adj_list[node]:
-            if nei == prev:
+        self.visited.add(curr_node)
+
+        for neighbor in adj_list[curr_node]:
+            if neighbor == prev_node:
                 continue
 
-            if not self.dfs(nei, node, adj_list):
+            if not self.dfs(neighbor, curr_node, adj_list):
                 return False
 
         return True
-            
+
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges) >= n:
-            return False
-
         adj_list = {}
-
         for i in range(n):
             adj_list[i] = []
 
-        for node, nei in edges:
-            adj_list[node].append(nei)
-            adj_list[nei].append(node)
+        for node, neighbor in edges:
+            adj_list[node].append(neighbor)
+            adj_list[neighbor].append(node)
 
         if not self.dfs(0, -1, adj_list):
             return False
 
-        if len(self.visited) != n:
-            return False
-
-        return True
+        return len(self.visited) == n
