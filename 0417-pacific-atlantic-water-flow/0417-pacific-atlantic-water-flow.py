081c59f6933cdd116e499afef9a5c2eb8e3c6a07
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-12-23 11:46:57 -0500

    Time: 787 ms (8.5%), Space: 19.5 MB (5.99%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,58 +1,49 @@
 class Solution:
-    def canReachOcean(self, row, col, grid):
-        cell_q = deque()
-        cell_q.append((row, col))
-
+    def get_cells_can_reach(self, row, col, heights):
         visited = set()
         visited.add((row, col))
 
-        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        cell_q = deque()
+        cell_q.append((row, col))
+
         while cell_q:
             old_row, old_col = cell_q.popleft()
+            directions = [[-1, 0], [0, -1], [0, 1], [1, 0]]
             for dr, dc in directions:
                 new_row, new_col = old_row + dr, old_col + dc
-                if new_row not in range(len(grid)) or \
-                    new_col not in range(len(grid[0])) or \
-                    (new_row, new_col) in visited or \
-                    grid[new_row][new_col] < grid[old_row][old_col]:
+
+                if new_row not in range(len(heights)) \
+                    or new_col not in range(len(heights[0])) \
+                    or (new_row, new_col) in visited \
+                    or heights[new_row][new_col] < heights[old_row][old_col]:
                     continue
 
-                cell_q.append((new_row, new_col))
                 visited.add((new_row, new_col))
+                cell_q.append((new_row, new_col))
 
         return visited
 
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        m = len(heights)
-        n = len(heights[0])
+        pacific_ocean_boundary = set()
+        atlantic_ocean_boundary = set()
 
-        pacific_ocean_islands = []
-        for col in range(n):
-            pacific_ocean_islands.append([0, col])
-
-        for row in range(1, m):
-            pacific_ocean_islands.append([row, 0])
-
-        atlantic_ocean_islands = []
-        for col in range(0, n):
-            atlantic_ocean_islands.append([m - 1, col])
-
-        for row in range(0, m-1):
-            atlantic_ocean_islands.append([row, n - 1])
-            
-        pacific_cells = set()
-        for row, col in pacific_ocean_islands:
-            pacific_cells.update(self.canReachOcean(row, col, heights))
-
-        atlantic_cells = set()
-        for row, col in atlantic_ocean_islands:
-            atlantic_cells.update(self.canReachOcean(row, col, heights))
-
-        result = []
         for row in range(len(heights)):
-            for col in range(len(heights[0])):
-                if (row, col) in pacific_cells and \
-                    (row, col) in atlantic_cells:
-                    result.append([row, col])
-        
-        return result
+            pacific_ocean_boundary.add((row, 0))
+            atlantic_ocean_boundary.add((row, len(heights[0]) - 1))
+
+        for col in range(len(heights[0])):
+            pacific_ocean_boundary.add((0, col))
+            atlantic_ocean_boundary.add((len(heights) - 1, col))
+
+        cells_reach_pacific = set()
+        for row, col in pacific_ocean_boundary:
+            cells_reach_pacific.update(self.get_cells_can_reach(row, col, heights))
+
+        cells_reach_atlantic = set()
+        for row, col in atlantic_ocean_boundary:
+            cells_reach_atlantic.update(self.get_cells_can_reach(row, col, heights))
+
+        result = set()
+        result = cells_reach_pacific.intersection(cells_reach_atlantic)
+
+        return list(result)
