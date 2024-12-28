f40f004ae48dcf9bc63ae2d80112ec8164332ecf
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-12-27 21:01:45 -0500

    Time: 446 ms (76.02%), Space: 43.7 MB (23.48%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,15 +1,13 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        num_subsequence = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
 
-        # dp = [[0] * (len(text2) + 1)] * (len(text1) + 1)
-        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
-        print(dp, len(dp), len(dp[0]))
-
-        for i in range(len(text1) - 1, -1, -1):
-            for j in range(len(text2) - 1, -1, -1):
-                if text1[i] == text2[j]:
-                    dp[i][j] = 1 + dp[i+1][j+1]
+        for row in range(len(num_subsequence) - 2, -1, -1):
+            for col in range(len(num_subsequence[0]) - 2, -1, -1):
+                if text1[row] == text2[col]:
+                    num_subsequence[row][col] = 1 + num_subsequence[row + 1][col + 1]
                 else:
-                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
-        
-        return dp[0][0]
+                    num_subsequence[row][col] = max(num_subsequence[row + 1][col], \
+                                                    num_subsequence[row][col + 1])
+
+        return num_subsequence[0][0]
