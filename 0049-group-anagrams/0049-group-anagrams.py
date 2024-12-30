f3334f8c96b16604a62494e0c23ef4d87b5c3526
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2024-12-30 14:20:23 -0500

    Time: 23 ms (20.1%), Space: 22.8 MB (6.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -7,18 +7,10 @@ class Solution:
             for char in word:
                 key[ord(char) - ord('a')] += 1
 
-            # anagram_key = ''.join(str(k) for k in key) # Always use tuple for 
-                                                         # dict keys because they 
-                                                         # are hashable and immutable. 
-                                                         # List cannot be used. 
-                                                         # Here 10 is being treated 
-                                                         # as 1 and 0 producing the same 
-                                                         # key for ["bdddddddddd","bbbbbbbbbbc"].
-            anagram_key = tuple(key)
-            
-            if anagram_key not in anagram_map:
-                anagram_map[anagram_key] = []
+            if tuple(key) not in anagram_map:
+                anagram_map[tuple(key)] = []
 
-            anagram_map[anagram_key].append(word)
+            anagram_map[tuple(key)].append(word)
 
         return list(anagram_map.values())
+            
