6c3c80cc836bb792c0cb9ca4a39b9bf766708be7
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2025-01-02 21:42:37 -0500

    Time: 1 ms (14.61%), Space: 17.8 MB (23.14%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -3,21 +3,16 @@ class Solution:
         self.all_subsets = []
         self.subset = []
 
-    def dfs(self, index, nums):
-        if index >= len(nums):
-            self.all_subsets.append(self.subset.copy())
-            return
-
-        self.subset.append(nums[index])
-        self.dfs(index + 1, nums)
-
-        self.subset.pop()
-        self.dfs(index + 1, nums)
-
-        return
-
+    def dfs(self, start, nums):
+        # Add the current subset to the list of all subsets
+        self.all_subsets.append(self.subset.copy())
+        
+        # Explore further subsets starting from the current index
+        for i in range(start, len(nums)):
+            self.subset.append(nums[i])  # Include nums[i] in the subset
+            self.dfs(i + 1, nums)       # Recur with the next index
+            self.subset.pop()           # Backtrack by removing nums[i]
+    
     def subsets(self, nums: List[int]) -> List[List[int]]:
         self.dfs(0, nums)
         return self.all_subsets
-
-        
