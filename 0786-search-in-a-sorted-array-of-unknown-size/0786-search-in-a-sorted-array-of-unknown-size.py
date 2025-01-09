20369e1dff755e0a72a4fecb3c2d1e98e434c62a
Akarshi Jain <69597477+akarshijain@users.noreply.github.com>
2025-01-08 22:03:59 -0500

    Time: 43 ms (47.47%), Space: 18.8 MB (28.8%) - LeetHub

---
+++ 0786-search-in-a-sorted-array-of-unknown-size/0786-search-in-a-sorted-array-of-unknown-size.py
@@ -0,0 +1,35 @@
+# """
+# This is ArrayReader's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class ArrayReader:
+#    def get(self, index: int) -> int:
+
+class Solution:
+    def search(self, reader: 'ArrayReader', target: int) -> int:
+        first_out_of_bound_index = 1
+
+        while reader.get(first_out_of_bound_index) != 2**31 - 1:
+            first_out_of_bound_index *= 2
+
+        left, right = 0, first_out_of_bound_index - 1
+        while left <= right:
+            mid = (left + right) // 2
+            value = reader.get(mid)
+
+            if value == 2**31 - 1:
+                right = mid - 1
+                continue
+
+            if value == target:
+                return mid
+
+            if value > target:
+                right = mid - 1
+
+            else:
+                left = mid + 1
+
+        return -1
+
+            
